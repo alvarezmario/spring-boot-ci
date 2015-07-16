304b6a1ee6e49158d9bd3f18943d205442710466
Mario Alvarez <malvarez@vimeworks.com>
2015-07-16 12:34:13 -0500

    Add a test

--- src/test/java/org/jenkins/JenkinsApplicationTests.java
+++ src/test/java/org/jenkins/JenkinsApplicationTests.java
@@ -1,10 +1,11 @@
 package org.jenkins;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = JenkinsApplication.class)
@@ -13,6 +14,8 @@ public class JenkinsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		Assert.assertEquals("Mario", "Mario");
 	}
 
 }
